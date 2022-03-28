053a04a976b73204fa84c3d5c55d2fffba450b0f
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-03-28 11:32:19 -0600

    first commit

---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Formulario from '../components/Formulario'
+
+const EditarCliente = () => {
+  
+  
+  const { id } = useParams()
+  const [cliente, setCliente] = useState({})
+  const [cargando, setCargando] = useState(true)
+
+  useEffect(() => {
+      const obtenerClienteAPI = async () => {
+          try {
+              const url = `http://localhost:4000/clientes/${id}`
+              const respuesta = await fetch(url)
+              const resultado = await respuesta.json()
+              setCliente(resultado)
+          } catch (error) {
+              console.log(error)
+          }
+          setTimeout(() => {
+              setCargando(!cargando)
+          }, 1000)
+      }
+      obtenerClienteAPI()
+  }, [])
+  
+  return (
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Editar Cliente</h1>
+      <p className='mt-3'>Utiliza este formulario para editar los datos del cliente</p>
+
+      {
+        cliente?.nombre ? (
+          <Formulario 
+            cliente={cliente}
+            cargando={cargando}
+          />
+        ): <p>Cliente Id no válido</p>
+      }
+    </>
+  )
+}
+
+export default EditarCliente
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
